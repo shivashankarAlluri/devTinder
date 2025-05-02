90c9a673b2253c1cf9d8c25338417d0f6d2cd95e
shivashankarAlluri <ashivashankar1211@gmail.com>
2025-05-02 12:29:49 +0530

    add

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const connectDB=async()=>{
-   await mongoose.connect("mongodb+srv://shivashankar:JtQDlFFCnBKW3Yka@namastenode.u6kr9.mongodb.net/devtinder");
+   await mongoose.connect(process.env.MONGO_URL);
 }
 
 module.exports=connectDB;
